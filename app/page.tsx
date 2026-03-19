77ab6194ceb328d12d256af16409708c252b1621
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-19 23:28:06 +0500

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
+import Testimonials from '@/components/Testimonials'
+import CTA from '@/components/CTA'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Hero />
+      <Features />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
